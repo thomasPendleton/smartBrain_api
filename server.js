b64c82cd567024dfa2e09617374858b59b3534d9
Thomas Pendleton <thomas6576@yahoo.com>
2022-12-07 13:29:02 -0800

    adding port

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.put('/image', (req, res) => { getImage(req, res, knex) })
 
 app.post('/imageurl', (req, res) => { handleApiCall(req, res) })
 
-app.listen(3000, () => {
-    console.log('app is running on 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on ${process.env.PORT || 3000}`);
 })
